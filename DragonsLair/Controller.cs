4244020f0d56fe16e3a655010678679cfeae2c26
Mikkel Jørgensen <mikk149c@edu.eal.dk>
2018-10-29 13:27:32 +0100

    *

--- DragonsLair/Controller.cs
+++ DragonsLair/Controller.cs
@@ -141,7 +141,13 @@ namespace DragonsLair
 		}
 
 
-		public void SaveMatch(string tournamentName, int roundNumber, string team1, string team2, string winningTeam)
+		public void SaveMatch(
+			string tournamentName,
+			int roundNumber,
+			string team1,
+			string team2,
+			string winningTeam
+			)
 		{
 			// Do not implement this method
 		}
